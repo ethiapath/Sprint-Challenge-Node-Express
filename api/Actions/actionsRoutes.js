a7966099cf9769c65b3f8fe49f1eb2dfe426f0bc
Hargobind Atwal <ethaipath@gmail.com>
2018-11-09 10:58:41 -0800

    added action and project endponts

--- api/Actions/actionsRoutes.js
+++ api/Actions/actionsRoutes.js
@@ -1,6 +1,95 @@
 const express = require('express');
+const actionsDB = require('../../data/helpers/actionModel.js')
 
 const router = express.Router();
+
+const getAllActions = (req, res) => {
+  actionsDB.get()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(500).json({ message: `Internal server error. Could not get actions`, error });
+    });
+}
+
+const getAction = (req, res) => {
+  actinosDB.get(req.params.id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res.status(500).json({ message: `Internal server error. Could not get actions`, error });
+    });
+}
+
+const addAction = (req, res) => {
+  process.stdout.write(req.body.name + '\n');
+  const { name } = req.body;
+  actionsDB.insert({ name })
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(error => {
+      res.status(500).json({ message: `Internal server error. Could not add action`, error });
+    });
+}
+
+const deleteAction = (req, res) => {
+  actionsDB.remove(req.params.id)
+    .then(actionsDeleted => {
+      if (actionsDeleted > 0) {
+        res.status(200).json(actionsDeleted);
+      } else {
+        res.status(400).json({ message: `action not deleted because they do not exist`, actionsDeleted});
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: `Internal server error. Could not delete action`, error });
+    });
+
+}
+
+const updateAction = (req, res) => {
+  if (req.body.name === undefined) {
+    res.status(400).json({ errorMessage: "Please provide a name for an action." });
+    return;
+  }
+  actionsDB.update(req.params.id, req.body)
+    .then(actionsUpdated => {
+      if (actionsUpdated > 0) {
+        res.status(200).json({ message: `${actionsUpdated} actions updated`});
+      } else {
+        res.status(404).json({ message: 'error updating action', error})
+      } 
+    })
+    .catch(error => {
+      res.status(500).json({ message: `Internal server error. Could not update action`, error });
+    });
+
+}
+
+const echo = (req, res) => {
+  res.status(201).json({
+    message: 'hey this endpoint work!',
+    params: req.params,
+    query: (req.query ? req.query : ''),
+    body: req.body
+  });
+}
+
+// endpoints
+
+// bind action endpoints
+router.get('/', getAllActions);
+router.get('/:id', getAction);
+router.post('/', addAction);
+router.delete('/:id', deleteAction);
+router.put('/:id', updateAction);
+
+
+
+
 
 
 module.exports = router;
--- api/Projects/projectsRoutes.js
+++ api/Projects/projectsRoutes.js
@@ -1,6 +1,152 @@
 const express = require('express');
+const projectDB = require('../../data/helpers/projectModel.js')
 
 const router = express.Router();
 
+
+
+/*
+ * getAllProjects
+ * GET - /api/projects 	
+ * Returns an array of all the project objects contained in the database.
+ * 
+  * */
+ const getAllProjects = (req, res, next) => {
+  projectDB.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ 
+          message: "The projects could not be retrieved." 
+        });
+    })
+}
+
+/* 
+ * getProjectById 
+ *
+ * GET - /api/projects/:id
+ * Returns the project object with the specified id.
+ * 
+  * */
+const getProjectById = (req, res, next) => {
+  projectDB.get(req.params.id)
+    .then(project => {
+      console.log(project)
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ 
+          message: `The project with id: ${req.params.id} does not exist`
+        });      
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ 
+          message: `The project with id: ${req.params.id} could not be retrieved.`
+        });
+    })
+}
+
+/*
+ * deleteProject
+ * DELETE - /api/projects/:id
+ * Removes the project with the specified id and returns the deleted project. 
+ *
+  * */
+const deleteProject =  async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const recordsDeleted = await projectDB.remove(id);
+    if (recordsDeleted > 0) {
+      res.status(200).json(recordsDeleted);
+    } else {
+      res.status(404).json({ 
+        message: `Could not delete record because it does not exist`
+      })
+    }
+  } catch (error) {
+    res.status(500).json({ message: "The project could not be removed", error })
+  }
+}
+
+/*
+ * addNewProject 
+ *
+ * POST - /api/projects  
+ * Creates a project using the information sent inside the request body. 
+ *
+  * */
+const addNewProject = (req, res, next) => {
+  if (req.body.userId === undefined || req.body.text === undefined) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the project." });
+    return;
+  }
+
+  projectDB.insert(req.body)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "There was an error while saving the project to the database" });
+    })
+}
+
+/*
+ * updateProject
+ *
+ * Updates the project with the specified id using data from the request body. 
+ * Returns the modified document, NOT the original.
+ *
+  * */
+const updateProject = (req, res, next) => {
+  if (req.body.text === undefined) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the project." });
+    return;
+  }
+
+  projectDB.update(req.params.id, req.body).then(count => {
+    if (count > 0) {
+      projectDB.get(req.params.id).then(project => {
+        console.log(project, project.hasOwnProperty('length'), project.length > 0)
+        res.status(200).json(project);
+      });
+    } else {
+      res.status(404).json({
+        message: `The project with the specified ID ${req.params.id} does not exist.`
+      })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ error: "The project information could not be modified." })
+  })
+}
+
+const getProjectActions = (req, res, next) => {
+  const { id } = req.params;
+  actionsDB.getProjectActions(id)
+    .then(actions => {
+      res.status(201).json(actions);
+    })
+    .catch(error => {
+      res.status(500).json({ message: `Could not find projects actions with ${id}`, error });
+    });
+}
+
+// bind project endpoints
+router.get('/', getAllProjects);
+router.get('/:id', getProjectById);
+router.post('/', addNewProject);
+router.delete('/:id', deleteProject);
+router.put(':id', updateProject);
+
+router.get('/:id/actions', getProjectActions);
 
 module.exports = router;
